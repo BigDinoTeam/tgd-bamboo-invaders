e215104a3b7696a02db2bf7d1b70be015494a2cf
David Forlen <david.forlen@laposte.net>
2020-05-23 12:26:16 +0200

    Correction des imports encore foutus en l'air par Intellij !

--- src/games/bambooInvaders/World.java
+++ src/games/bambooInvaders/World.java
@@ -1,6 +1,7 @@
 package games.bambooInvaders;
 
 import app.AppLoader;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
